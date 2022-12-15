1a304d6dd71b50f4640e2c7eac7876f2e0e37951
javiersarribas <92797813+javiersarribas@users.noreply.github.com>
2022-12-15 22:25:03 +0000

    App updates

--- Server Side gRPC (Doctor Directory)/client/app.js
+++ Server Side gRPC (Doctor Directory)/client/app.js
@@ -8,10 +8,13 @@ var contacts = grpc.loadPackageDefinition(packageDefinition).contacts
 
 var client = new contacts.ContactService("0.0.0.0:40000", grpc.credentials.createInsecure());
 
-var call = client.getPhoneNumber({ })
+var call = client.getContact({ })
+
+console.log("-------------------------------------------------------\nDoctor Phone Number Directory (Name, Section, Number):\n-------------------------------------------------------")
+
 
 call.on('data', (response) => { /*=> Same as function, or "anonymous function", arrow function*/
-  console.log("The phone number of Dr. " + response.contactName + "from WHATEVERPLACE is the following: " + response.phoneNumber)
+  console.log("Dr. " + response.contactName + ", from " + response.section + " : " + response.phoneNumber)
 })
 
 call.on('end', function() {
@@ -21,30 +24,3 @@ call.on('end', function() {
 call.on('error', function(e) {
   console.log(e)
 })
-
-
-/*
-var grpc = require("@grpc/grpc-js")
-var protoLoader = require("@grpc/proto-loader")
-var PROTO_PATH = __dirname + "/protos/movies.proto"
-
-var packageDefinition = protoLoader.loadSync(PROTO_PATH)
-
-var movies = grpc.loadPackageDefinition(packageDefinition).movies
-
-var client = new movies.MovieService("0.0.0.0:40000", grpc.credentials.createInsecure());
-
-var call = client.getFavouriteMovies({ })
-
-call.on('data', (response) => { => Same as function, or "anonymous function", arrow function
-  console.log(response.favouriteMovie + " people chose " + response.movieType + " as their favourite movie!")
-})
-
-call.on('end', function() {
-
-})
-
-call.on('error', function(e) {
-  console.log(e)
-})
-*/
--- Server Side gRPC (Doctor Directory)/server/app.js
+++ Server Side gRPC (Doctor Directory)/server/app.js
@@ -1,47 +1,79 @@
 var grpc = require("@grpc/grpc-js")
 var protoLoader = require("@grpc/proto-loader")
 var PROTO_PATH = __dirname + "/protos/contacts.proto"
-var packageDefinition = protoLoader.loadSync(
-  PROTO_PATH
-)
+
+var packageDefinition = protoLoader.loadSync(PROTO_PATH)
 var contacts_proto = grpc.loadPackageDefinition(packageDefinition).contacts
 
 var data = [
   {
     contactName:"James McCarthy",
-    phoneNumber: 089568995
+    phoneNumber: 35389568995,
+    section: "General medicine"
   },
   {
     contactName:"Lucy Miles",
-    phoneNumber: 087445986
+    phoneNumber: 35387445986,
+    section: "Urology"
   },
   {
     contactName:"Marija Stribl",
-    phoneNumber: 087326998
+    phoneNumber: 35387326998,
+    section: "Pediatry"
   },
 
   {
     contactName:"Alberto Ortiz",
-    phoneNumber: 0874512365
+    phoneNumber: 353874512365,
+    section: "Odontology"
   },
   {
     contactName:"Andrew Wall",
-    phoneNumber: 0896532245
+    phoneNumber: 353896532245,
+    section: "Dental"
+  },
+  {
+    contactName:"David Bucur",
+    phoneNumber: 353875485523,
+    section: "Psychology"
+  },
+  {
+    contactName:"Karina Pysz",
+    phoneNumber: 353886258874,
+    section: "Oncology"
+  },
+  {
+    contactName:"Marika Myerkevicz",
+    phoneNumber: 353985634584,
+    section: "Dietist"
+  },
+  {
+    contactName:"Adam Connolly",
+    phoneNumber: 353876585547,
+    section: "Blood Specialist"
+  },
+  {
+    contactName:"Laura Kelly",
+    phoneNumber: 353894548998,
+    section: "Dermathologist"
   }
 ]
 
-function getPhoneNumber(call, callback) {
+function getContact(call, callback) {
   for(var i = 0; i < data.length; i++) {
     call.write({
       contactName: data[i].contactName,
-      phoneNumber: data[i].phoneNumber
+      phoneNumber: data[i].phoneNumber,
+      section: data[i].section
     })
   }
   call.end()
 }
 
 var server = new grpc.Server()
-server.addService(contacts_proto.ContactService.service, { getPhoneNumber: getPhoneNumber})
+server.addService(contacts_proto.ContactService.service, {
+  getContact: getContact
+})
 server.bindAsync("0.0.0.0:40000", grpc.ServerCredentials.createInsecure(), function() {
   server.start()
 })
